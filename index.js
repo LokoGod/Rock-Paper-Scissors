c4356b4b95e283d708bbd812e14fcf22a18c2161
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-11-22 17:57:20 +0530

    math aint mathing

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const getComputerChoice = () => {
-    console.log("Hello, There!");
+    const options = ['Rock', 'Paper', 'Scissors'];
+    return options[(Math.floor(Math.random() * options.length))]
 }
+
+console.log(getComputerChoice());
+
+
+console.log("Result : " + Math.floor(-89.02));
 
