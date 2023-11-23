4e8bc3f51e713a3c22469d65bb29b495f9007fe6
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-11-23 14:05:20 +0530

    sc

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
+// Function to get computer choice
 const getComputerChoice = () => {
     const options = ['Rock', 'Paper', 'Scissors'];
     return options[(Math.floor(Math.random() * options.length))]
 }
 
+// Function to play a single round
 const playRound = (playerSelection, computerSelection) = () => {
+    // Make playerSelection case-insensitive
+    const playerChoice = playerSelection.toLowerCase();
+
+    if (playerChoice === computerSelection) {
+        return "It's a tie!"
+    } else if ((playerChoice === 'Rock' && computerSelection === 'Scissors') ||
+     (playerChoice === 'Paper' && computerSelection === 'Rock') ||
+      (playerChoice === 'Scissors' && computerSelection === 'Paper')) {
+        return `You win! ${playerChoice} beats ${computerSelection}`
+    } else {
+        return `You lose! ${computerSelection} beats ${playerChoice}`
+    }
+}
+
+const game = () => {
     
 }
+
 
 const playerSelection = 'rock'
 const computerSelection = getComputerChoice()
